a694c72927e158984b66aa8ac66c5f2543d8da17
gabrielbezdev <gabrielbvl2024@gmail.com>
2024-07-16 12:04:30 -0300

    feat: project creation and tailwind configuration

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header"
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+      <div>
+        <h1 className='font-bold underline hover:text-orange-600'>Hello Word</h1>
+        <h1 className='font-bold text-blue-600 underline hover:text-yellow-600'>Hello Word</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+      <div>
+        <h1 className="font-bold text-purple-600 text-xl hover:text-3xl">Test Header</h1>
+      </div>
+    );
+  }
+  
+  export default Header;
